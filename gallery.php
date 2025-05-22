cac0409de01a32f08841625e9b548a47cbb54c1b
Noemi Tortora <124146319+noemi21@users.noreply.github.com>
2025-05-22 14:50:51 +0200

    Sito aggiornato con la nuova implementazione
    Ho messo online la nuova versione del sito.
    È stata completamente ricostruita da zero con una struttura più ordinata, codice più pulito e una migliore organizzazione generale.

---
+++ gallery.php
@@ -0,0 +1,649 @@
+<?php
+
+$directory = 'ArCommerciale/';
+
+$images = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+?>
+
+<?php
+
+$directory = 'Clean/';
+
+$ims = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+?>
+
+<?php
+
+$directory = 'Manutenzione/';
+
+$imag = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+?>
+
+<?php
+
+$directory = 'Carpenteria/';
+
+$img = glob($directory . "*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+
+?>
+
+<!DOCTYPE html>
+
+<html style="font-size: 16px;" lang="it">
+
+
+
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta charset="utf-8">
+
+  <meta name="keywords" content="Arredamento Commerciale, Carpenteria Metallica​, Clean Room, Manutenzioni​">
+
+  <meta name="description" content="">
+
+  <meta name="google-site-verification" content="S_yYhOjMog8jMFIK-xJePMCJRyJRrnP8Lb8AYaN2mag" />
+
+  <link rel="icon" type="image/x-icon" href="images/FemoLogo.png">
+
+  <title>Gallery</title>
+
+  <link rel="stylesheet" href="nicepage.css" media="screen">
+
+  <link rel="stylesheet" href="gallery.css" media="screen">
+
+  <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+
+  <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+
+  <meta name="generator" content="Nicepage 6.21.3, nicepage.com">
+
+  <link id="u-theme-google-font" rel="stylesheet"
+
+    href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+
+  <link id="u-page-google-font" rel="stylesheet"
+
+    href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <meta name="theme-color" content="#488bca">
+
+  <meta property="og:title" content="Gallery">
+
+  <meta property="og:type" content="website">
+
+  <meta data-intl-tel-input-cdn-path="intlTelInput/">
+
+  <style>
+
+    body {
+
+      font-family: 'Montserrat', sans-serif;
+
+      margin: 0;
+
+      padding: 0;
+
+      height: 2000px;
+
+      background-color: #f4f4f4;
+
+    }
+
+
+
+    .u-effect-fade {
+
+      opacity: 1 !important;
+
+    }
+
+
+
+    .scroll-to-top {
+
+      position: fixed;
+
+      bottom: 60px;
+
+      right: 20px;
+
+      background-color: #000;
+
+      color: #fff;
+
+      border: none;
+
+      padding: 10px 20px;
+
+      border-radius: 5px;
+
+      cursor: pointer;
+
+      display: none;
+
+      z-index: 1000;
+
+    }
+
+
+
+    .scroll-to-top:hover {
+
+      background-color: #555;
+
+    }
+
+
+
+    .index-container {
+
+      margin-bottom: 20px;
+
+      display: flex;
+
+      justify-content: space-around;
+
+      flex-wrap: wrap;
+
+    }
+
+
+
+    .index-container ul {
+
+      list-style-type: none;
+
+      padding: 0;
+
+      display: flex;
+
+      gap: 20px;
+
+      flex-wrap: wrap;
+
+      justify-content: center;
+
+    }
+
+
+
+    .index-container ul li {
+
+      margin: 5px 0;
+
+    }
+
+
+
+    .index-container ul li a {
+
+      text-decoration: none;
+
+      color: #000;
+
+      font-size: 18px;
+
+    }
+
+
+
+    .index-container ul li a:hover {
+
+      text-decoration: underline;
+
+    }
+
+
+
+    @media (max-width: 600px) {
+
+      .index-container ul {
+
+        flex-direction: column;
+
+        align-items: center;
+
+      }
+
+
+
+      .index-container ul li a {
+
+        font-size: 16px;
+
+      }
+
+    }
+
+  </style>
+
+  
+
+  <link rel="preload" as="script" href="https://cdn.iubenda.com/cs/iubenda_cs.js" />
+
+  <link rel="preload" as="script" href="https://cdn.iubenda.com/cs/tcf/stub-v2.js" />
+
+  <script src="https://cdn.iubenda.com/cs/tcf/stub-v2.js"></script>
+
+  <script>
+
+    (_iub = self._iub || []).csConfiguration = {
+
+      cookiePolicyId: 35214885,
+
+      siteId: 3743016,
+
+      timeoutLoadConfiguration: 30000,
+
+      lang: 'it',
+
+      enableTcf: true,
+
+      tcfVersion: 2,
+
+      tcfPurposes: {
+
+        "2": "consent_only",
+
+        "3": "consent_only",
+
+        "4": "consent_only",
+
+        "5": "consent_only",
+
+        "6": "consent_only",
+
+        "7": "consent_only",
+
+        "8": "consent_only",
+
+        "9": "consent_only",
+
+        "10": "consent_only"
+
+      },
+
+      invalidateConsentWithoutLog: true,
+
+      googleAdditionalConsentMode: true,
+
+      consentOnContinuedBrowsing: false,
+
+      banner: {
+
+        position: "top",
+
+        acceptButtonDisplay: true,
+
+        customizeButtonDisplay: true,
+
+        closeButtonDisplay: true,
+
+        closeButtonRejects: true,
+
+        fontSizeBody: "14px",
+
+      },
+
+    }
+
+  </script>
+
+  <script async src="https://cdn.iubenda.com/cs/iubenda_cs.js"></script>
+
+</head>
+
+
+
+<body data-path-to-root="./" data-include-products="true" class="u-body u-xl-mode" data-lang="it">
+
+
+
+<header class="u-clearfix u-header u-header" id="sec-a056">
+
+    <div class="u-clearfix u-sheet u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+
+      <a href="./" class="u-image u-logo u-image-1" data-image-width="509" data-image-height="490">
+
+        <img src="images/FemoLogo.png" class="u-logo-image u-logo-image-1">
+
+      </a>
+
+      <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+
+        <div class="menu-collapse u-custom-font u-font-montserrat"
+
+          style="font-size: 1rem; letter-spacing: 0px; font-weight: 700;">
+
+          <a class="u-button-style u-custom-border u-custom-border-color u-custom-borders u-custom-color u-custom-effect-duration u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-text-shadow u-custom-text-shadow-blur u-custom-text-shadow-color u-custom-text-shadow-transparency u-custom-text-shadow-x u-custom-text-shadow-y u-custom-top-bottom-menu-spacing u-hamburger-link u-nav-link u-text-active-black u-text-black u-text-hover-black"
+
+            href="#">
+
+            <svg class="u-svg-link" viewBox="0 0 24 24">
+
+              <use xlink:href="#menu-hamburger"></use>
+
+            </svg>
+
+            <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px"
+
+              xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+
+              <g>
+
+                <rect y="1" width="16" height="2"></rect>
+
+                <rect y="7" width="16" height="2"></rect>
+
+                <rect y="13" width="16" height="2"></rect>
+
+              </g>
+
+            </svg>
+
+          </a>
+
+        </div>
+
+        <div class="u-custom-menu u-nav-container">
+
+          <ul class="u-custom-font u-font-montserrat u-nav u-spacing-20 u-unstyled u-nav-1">
+
+            <li class="u-nav-item"><a
+
+                class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="./"
+
+                style="padding:10px;">Home</a>
+
+            </li>
+
+            <li class="u-nav-item"><a
+
+                class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="/index.html#servizi"
+
+                style="padding: 10px;">Servizi</a>
+
+            </li>
+
+            <li class="u-nav-item"><a
+
+                class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="/index.html#chisiamo"
+
+                style="padding: 10px;">Chi siamo</a>
+
+            </li>
+
+            <li class="u-nav-item"><a
+
+                class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black"
+
+                href="gallery.php" style="padding: 10px;">Gallery</a>
+
+            </li>
+
+            <li class="u-nav-item"><a
+
+                class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="/index.html#contatti"
+
+                style="padding: 10px;">Contatti</a>
+
+            </li>
+
+            <li class="u-nav-item"><a
+
+              class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="gallery.php"
+
+              style="padding: 10px;">IT</a>
+
+          </li>
+
+          <li class="u-nav-item"><a
+
+            class="u-button-style u-nav-link u-text-active-black u-text-grey-90 u-text-hover-black" href="gallery_eng.php"
+
+            style="padding: 10px;">EN</a>
+
+        </li>
+
+          </ul>
+
+        </div>
+
+        <div class="u-custom-menu u-nav-container-collapse">
+
+          <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+
+            <div class="u-inner-container-layout u-sidenav-overflow">
+
+              <div class="u-menu-close"></div>
+
+              <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="./">Home</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="/index.html#servizi">Servizi</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="/index.html#chisiamo">Chi siamo</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="gallery.html">Gallery</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="/index.html#contatti">Contatti</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="gallery.php">IT</a>
+
+                </li>
+
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="gallery_eng.php">EN</a>
+
+                </li>
+
+              </ul>
+
+            </div>
+
+          </div>
+
+          <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+
+        </div>
+
+      </nav>
+
+    </div>
+
+  </header>
+
+  <button class="scroll-to-top" onclick="scrollToTop()">Torna su</button>
+
+  <script>
+
+    window.onscroll = function() {
+
+      var scrollToTopButton = document.querySelector('.scroll-to-top');
+
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+        scrollToTopButton.style.display = "block";
+
+      } else {
+
+        scrollToTopButton.style.display = "none";
+
+      }
+
+    };
+
+
+
+    function scrollToTop() {
+
+      window.scrollTo({
+
+        top: 0,
+
+        behavior: 'smooth'
+
+      });
+
+    }
+
+  </script>
+
+  <div class="index-container">
+
+    <ul>
+
+      <li><a href="#paragrafo1" style="font-family: 'Montserrat', sans-serif;">Arredamento Commerciale</a></li>
+
+      <li><a href="#paragrafo2" style="font-family: 'Montserrat', sans-serif;">Clean Room</a></li>
+
+      <li><a href="#paragrafo3" style="font-family: 'Montserrat', sans-serif;">Manutenzioni</a></li>
+
+      <li><a href="#paragrafo4" style="font-family: 'Montserrat', sans-serif;">Carpenteria Metallica</a></li>
+
+    </ul>
+
+  </div>
+
+  <section id="paragrafo1" class="u-align-center u-clearfix u-container-align-center u-section-4" id="sec-8e52">
+
+    <div class="u-clearfix u-sheet u-sheet-1">
+
+      <h1 class="u-align-center u-custom-font u-font-montserrat u-text u-text-default u-text-1">Arredamento Commerciale</h1>
+
+      <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+
+        <div class="u-gallery-inner u-gallery-inner-1"> <?php foreach ($images as $image): ?> <div class="u-effect-fade u-gallery-item">
+
+              <div class="u-back-slide"> <img class="u-back-image u-expanded" src="<?php echo $image; ?>" alt="Descrizione Immagine" loading="lazy"> </div>
+
+              <div class="u-over-slide u-shading u-over-slide-1"></div>
+
+            </div> <?php endforeach; ?> </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="paragrafo2" class="u-align-center u-clearfix u-container-align-center u-section-4" id="sec-8e52">
+
+    <div class="u-clearfix u-sheet u-sheet-1">
+
+      <h2 class="u-align-center u-custom-font u-font-montserrat u-text u-text-default u-text-1">Clean Room</h2>
+
+      <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+
+        <div class="u-gallery-inner u-gallery-inner-1"> <?php foreach ($ims as $image): ?> <div class="u-effect-fade u-gallery-item">
+
+              <div class="u-back-slide"> <img class="u-back-image u-expanded" src="<?php echo $image; ?>" alt="Descrizione Immagine" loading="lazy"> </div>
+
+              <div class="u-over-slide u-shading u-over-slide-1"></div>
+
+            </div> <?php endforeach; ?> </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="paragrafo3" class="u-align-center u-clearfix u-container-align-center u-section-4" id="sec-8e52">
+
+    <div class="u-clearfix u-sheet u-sheet-1">
+
+      <h2 class="u-align-center u-custom-font u-font-montserrat u-text u-text-default u-text-1">Manutenzioni</h2>
+
+      <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+
+        <div class="u-gallery-inner u-gallery-inner-1"> <?php foreach ($imag as $image): ?> <div class="u-effect-fade u-gallery-item">
+
+              <div class="u-back-slide"> <img class="u-back-image u-expanded" src="<?php echo $image; ?>" alt="Descrizione Immagine" loading="lazy"> </div>
+
+              <div class="u-over-slide u-shading u-over-slide-1"></div>
+
+            </div> <?php endforeach; ?> </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <section id="paragrafo4" class="u-align-center u-clearfix u-container-align-center u-section-4" id="sec-8e52">
+
+    <div class="u-clearfix u-sheet u-sheet-1">
+
+      <h2 class="u-align-center u-custom-font u-font-montserrat u-text u-text-default u-text-1">Carpenteria Metallica</h2>
+
+      <div class="u-expanded-width u-gallery u-layout-grid u-lightbox u-show-text-on-hover u-gallery-1">
+
+        <div class="u-gallery-inner u-gallery-inner-1"> <?php foreach ($img as $image): ?> <div class="u-effect-fade u-gallery-item">
+
+              <div class="u-back-slide"> <img class="u-back-image u-expanded" src="<?php echo $image; ?>" alt="Descrizione Immagine" loading="lazy"> </div>
+
+              <div class="u-over-slide u-shading u-over-slide-1"></div>
+
+            </div> <?php endforeach; ?> </div>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <footer
+
+    class="u-align-center u-clearfix u-container-align-center u-container-align-center-lg u-container-align-center-xl u-custom-color-1 u-footer u-footer"
+
+    id="sec-04c6">
+
+    <div class="u-clearfix u-sheet u-sheet-1">
+
+      <p
+
+        class="u-align-center u-custom-font u-font-montserrat u-small-text u-text u-text-variant u-text-white u-text-1">
+
+        © 2024 Femo Montaggi - Tutti i diritti riservati&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<a
+
+          href="https://www.iubenda.com/privacy-policy/35214885" target="_blank"
+
+          class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1">Privacy
+
+          Policy</a>&nbsp;&nbsp;<a href="https://www.femosrl.it/contatti.html#"
+
+          class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-2">Termini
+
+          e Condizioni&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</a>P.iva 07810280722
+
+      </p>
+
+    </div>
+
+  </footer>
+
+</body>
+
+
+
+</html>
